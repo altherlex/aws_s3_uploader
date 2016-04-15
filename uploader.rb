8d12628c2ce288fffab4f5a67e32e93eedc8db7d
Altherlex Alves <para.alves@gmail.com>
2016-04-15 17:15:11 -0300

    informating in finished proccess

--- uploader.rb
+++ uploader.rb
@@ -2,10 +2,12 @@ require "s3"
 
 class UploadS3
   PATH = '/home/grade'
+  BUCKET = 'espn-api-importer'
 
   def initilize
     @service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY'])
-    @bucket = @service.buckets.find("espn-api-importer")
+    @bucket = @service.buckets.find(BUCKET)
+    @counter = 0
   end
 
   def upload(file_name)
@@ -14,7 +16,12 @@ class UploadS3
     new_object = @bucket.objects.build("grade/#{file_name}")
     new_object.content = open( local_file_path )
     
-    File.delete(local_file_path) if new_object.save
+    if new_object.save
+      @counter += 1
+      File.delete(local_file_path)
+    end
+
+    puts "Success finished: #{@counter} files upload to #{BUCKET} bucket"
   end
 
   def run!
