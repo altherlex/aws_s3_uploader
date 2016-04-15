825208f34f93304b7bd63fbb4799e186a90ef936
Altherlex Alves <para.alves@gmail.com>
2016-04-15 17:02:36 -0300

    file list filter

--- uploader.rb
+++ uploader.rb
@@ -5,13 +5,14 @@ service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_ac
 
 bucket = service.buckets.find("espn-api-importer")
 
-Dir::glob(File.join(PATH, '*i.xml')).each do |file|
-  upload(file)
-end
-
 def upload(file_name)
   new_object = bucket.objects.build("grade/#{file_name}")
   new_object.content = open(File.join(PATH, file_name))
   new_object.save
 end
+
+Dir::glob(File.join(PATH, '*.xml')).each do |file|
+  upload(file)
+end
+
  
