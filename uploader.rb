8bb42f867653118f69188b91d2fe3dc09b632616
Altherlex Alves <para.alves@gmail.com>
2016-04-15 17:08:07 -0300

    OO

--- uploader.rb
+++ uploader.rb
@@ -1,18 +1,28 @@
 require "s3"
-PATH = '/home/grade'
 
-service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY'])
+class UploadS3
+  PATH = '/home/grade'
 
-bucket = service.buckets.find("espn-api-importer")
+  def initilize
+    @service = S3::Service.new(:access_key_id => ENV['AWS_ACCESS_KEY_ID'], :secret_access_key =>ENV['AWS_SECRET_ACCESS_KEY'])
+    @bucket = @service.buckets.find("espn-api-importer")
+  end
 
-def upload(file_name)
-  new_object = bucket.objects.build("grade/#{file_name}")
-  new_object.content = open(File.join(PATH, file_name))
-  new_object.save
+  def upload(file_name)
+    local_file_path = File.join(PATH, file_name)
+    
+    new_object = @bucket.objects.build("grade/#{file_name}")
+    new_object.content = open( local_file_path )
+    
+    File.delete(local_file_path) if new_object.save
+  end
+
+  def run!
+    Dir::glob(File.join(PATH, '*.xml')).each do |file|
+      upload(file)
+    end
+  end
 end
-
-Dir::glob(File.join(PATH, '*.xml')).each do |file|
-  upload(file)
-end
-
  
+up = UploadS3.new
+up.run!
